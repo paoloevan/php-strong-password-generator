5745ca34c8f23956516f27dca2068021424eef57
paoloevan <paoloev1900@gmail.com>
2022-11-29 13:44:39 +0100

    add length dump

--- index.php
+++ index.php
@@ -1,6 +1,43 @@
-/**
-Creare un form che invii in GET la lunghezza della password.
+<!-- Creare un form che invii in GET la lunghezza della password.
 Una nostra funzione utilizzerà questo dato per generare una password casuale
 (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
-Scriviamo tutto (logica e layout) in un unico file index.php
-*/
+Scriviamo tutto (logica e layout) in un unico file index.php -->
+<?php
+var_dump(strlen($_GET['password']));
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password generator</title>
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi' crossorigin='anonymous'>
+</head>
+
+<body>
+    <div class="container py-5">
+        <form action="index.php" method="GET" class="form">
+            <div class="row g-3 align-items-center">
+                <div class="col-auto">
+                    <label for="password" class="col-form-label">Password</label>
+                </div>
+                <div class="col-auto">
+                    <input type="text" id="password" name="password" class="form-control" aria-describedby="passwordHelpInline">
+                </div>
+                <div class="col-auto">
+                    <span id="passwordHelpInline" class="form-text">
+                        Must be 8-20 characters long.
+                    </span>
+                </div>
+                <div class="col-auto">
+                    <button class=" btn btn-primary" type="submit">Invia</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
