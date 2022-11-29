aa18b98658f1b2bd758a7e92417aa24e80da4cf6
paoloevan <paoloev1900@gmail.com>
2022-11-29 13:34:10 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,6 @@
+/**
+Creare un form che invii in GET la lunghezza della password.
+Una nostra funzione utilizzerà questo dato per generare una password casuale
+(composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
+Scriviamo tutto (logica e layout) in un unico file index.php
+*/
